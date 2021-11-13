3b1bb8f92d52db2cda878b26e1fa4078ed56c785
Arthur <artoni.az@gmail.com>
2021-11-13 19:30:49 +0100

    Fixed dialogs ui bugs

--- lib/screens/review/rating_dialogs/quick_rating_dialog.dart
+++ lib/screens/review/rating_dialogs/quick_rating_dialog.dart
@@ -10,6 +10,7 @@ class QuickRatingDialog extends StatelessWidget {
   final FocusNode chaletDescFocusNode;
   final Function createReview;
   final bool validateQuickReview;
+  final ScrollController scrollController;
   const QuickRatingDialog({
     Key? key,
     required this.handleRatingUpdate,
@@ -17,6 +18,7 @@ class QuickRatingDialog extends StatelessWidget {
     required this.chaletDescFocusNode,
     required this.createReview,
     required this.validateQuickReview,
+    required this.scrollController,
   }) : super(key: key);
 
   @override
@@ -25,49 +27,55 @@ class QuickRatingDialog extends StatelessWidget {
       contentPadding: EdgeInsets.zero,
       content: GestureDetector(
         onTap: () => dissmissCurrentFocus(context),
-        child: Container(
-          constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.8),
-          padding: EdgeInsets.fromLTRB(24.0, 50.0, 24.0, 24.0),
-          child: Form(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  'Twoja ocena szaletu',
-                  style: Theme.of(context).textTheme.headline2,
-                ),
-                VerticalSizedBox8(),
-                Text(
-                  'Oceń Twoje ogóle wrażenie z pobytu w tym szalecie',
-                  style: Theme.of(context).textTheme.bodyText2,
-                ),
-                VerticalSizedBox16(),
-                RatingBarItem(handleRatingUpdate: handleRatingUpdate),
-                VerticalSizedBox8(),
-                TextField(
-                    minLines: 2,
-                    maxLines: 4,
-                    controller: chaletDescController,
-                    focusNode: chaletDescFocusNode,
-                    decoration: textInputDecoration.copyWith(
-                      hintText: 'Opis',
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                        color: Colors.grey,
-                        width: 0.5,
-                      )),
-                    )),
-                if (validateQuickReview)
+        child: SingleChildScrollView(
+          controller: scrollController,
+          child: Container(
+            constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.8),
+            padding: EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 24.0),
+            child: Form(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
                   Text(
-                    'Uzupełnij ocenę i opis',
-                    style: Theme.of(context).textTheme.bodyText2!.copyWith(color: Colors.red),
+                    'Twoja ocena szaletu',
+                    style: Theme.of(context).textTheme.headline2,
+                    textAlign: TextAlign.center,
                   ),
-                VerticalSizedBox16(),
-                ButtonsPopUpRow(
-                  approveButtonLabel: 'Zapisz ocenę',
-                  onPressedApproveButton: createReview,
-                ),
-              ],
+                  VerticalSizedBox8(),
+                  Text(
+                    'Oceń Twoje ogóle wrażenie z pobytu w tym szalecie',
+                    style: Theme.of(context).textTheme.bodyText2,
+                    textAlign: TextAlign.center,
+                  ),
+                  VerticalSizedBox16(),
+                  RatingBarItem(handleRatingUpdate: handleRatingUpdate),
+                  VerticalSizedBox8(),
+                  TextField(
+                      minLines: 2,
+                      maxLines: 4,
+                      controller: chaletDescController,
+                      focusNode: chaletDescFocusNode,
+                      decoration: textInputDecoration.copyWith(
+                        hintText: 'Opis',
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                          color: Colors.grey,
+                          width: 0.5,
+                        )),
+                      )),
+                  if (validateQuickReview)
+                    Text(
+                      'Uzupełnij ocenę i opis',
+                      style: Theme.of(context).textTheme.bodyText2!.copyWith(color: Colors.red),
+                    ),
+                  VerticalSizedBox16(),
+                  ButtonsPopUpRow(
+                    approveButtonLabel: 'Zapisz ocenę',
+                    onPressedApproveButton: createReview,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
