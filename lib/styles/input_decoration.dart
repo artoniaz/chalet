2cbee8559aca1f5bd6ab622101fb9b56eefe83ad
Arthur <artoni.az@gmail.com>
2021-11-14 12:50:00 +0100

    Changed input decoration border bottom

--- lib/styles/input_decoration.dart
+++ lib/styles/input_decoration.dart
@@ -5,28 +5,28 @@ const textInputDecoration = InputDecoration(
     contentPadding: EdgeInsets.all(Dimentions.small),
     fillColor: Palette.backgroundWhite,
     filled: true,
-    border: OutlineInputBorder(
+    border: UnderlineInputBorder(
         borderSide: BorderSide(
       color: Palette.backgroundWhite,
-      width: 2.0,
+      width: 1.0,
     )),
-    enabledBorder: OutlineInputBorder(
+    enabledBorder: UnderlineInputBorder(
         borderSide: BorderSide(
-      color: Palette.backgroundWhite,
-      width: 2.0,
+      color: Palette.chaletBlue,
+      width: 1.0,
     )),
     focusedBorder: OutlineInputBorder(
         borderSide: BorderSide(
       color: Palette.goldLeaf,
-      width: 2.0,
+      width: 1.0,
     )),
     focusedErrorBorder: OutlineInputBorder(
         borderSide: BorderSide(
       color: Palette.goldLeaf,
-      width: 2.0,
+      width: 1.0,
     )),
     errorBorder: OutlineInputBorder(
         borderSide: BorderSide(
       color: Palette.errorRed,
-      width: 2.0,
+      width: 1.0,
     )));
