4089e78f75250ead494f380b86a1be0f927b0b0a
Arthur <artoni.az@gmail.com>
2021-11-14 12:34:14 +0100

    Changed ui for register and forgot password screens

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -52,63 +52,67 @@ class _RegisterState extends State<Register> {
         : GestureDetector(
             onTap: () => dissmissCurrentFocus(context),
             child: Scaffold(
-                appBar: CustomAppBars.customAppBarDark(context, 'Rejestracja'),
                 body: SingleChildScrollView(
-                  child: SafeArea(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: Dimentions.horizontalPadding, vertical: Dimentions.large),
-                      child: Form(
-                          key: _formKey,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              TextFormField(
-                                decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                                validator: (val) =>
-                                    val!.isEmpty || !val.contains('@') ? 'Podaj poprawny adres email' : null,
-                                onChanged: (String val) => setState(() => email = val),
-                                onEditingComplete: () => node.nextFocus(),
-                                keyboardType: TextInputType.emailAddress,
-                              ),
-                              VerticalSizedBox16(),
-                              TextFormField(
-                                controller: passwordController,
-                                decoration: textInputDecoration.copyWith(hintText: 'Hasło'),
-                                obscureText: true,
-                                validator: (val) => val!.length < 6 ? 'Hasło musi zawierać minimum 6 znaków' : null,
-                                onEditingComplete: () => registerWithEmailAndPassword(),
-                              ),
-                              VerticalSizedBox16(),
-                              TextFormField(
-                                controller: passwordRepeatController,
-                                decoration: textInputDecoration.copyWith(hintText: 'Powtórz hasło'),
-                                obscureText: true,
-                                validator: (val) =>
-                                    val != passwordController.text ? 'Podane hasła nie są zgodne' : null,
-                                onEditingComplete: () => registerWithEmailAndPassword(),
-                              ),
-                              VerticalSizedBox16(),
-                              CustomElevatedButton(
-                                label: 'Rejestracja',
-                                onPressed: registerWithEmailAndPassword,
-                              ),
-                              if (error.isNotEmpty) VerticalSizedBox16(),
-                              Text(
-                                error,
-                                style: Theme.of(context).textTheme.bodyText2!.copyWith(color: Palette.errorRed),
-                              ),
-                              if (error.isNotEmpty) VerticalSizedBox16(),
-                              CustomTextButton(
-                                onPressed: widget.toggleView,
-                                label: 'Masz już konto? Zaloguj się.',
-                                color: Palette.ivoryBlack,
-                              )
-                            ],
-                          )),
-                    ),
-                  ),
-                )),
+              child: SafeArea(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: Dimentions.horizontalPadding, vertical: Dimentions.large),
+                  child: Form(
+                      key: _formKey,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Image(
+                            width: 80.0,
+                            height: 80.0,
+                            image: AssetImage('assets/poo/poo_happy.png'),
+                          ),
+                          VerticalSizedBox16(),
+                          TextFormField(
+                            decoration: textInputDecoration.copyWith(hintText: 'Email'),
+                            validator: (val) =>
+                                val!.isEmpty || !val.contains('@') ? 'Podaj poprawny adres email' : null,
+                            onChanged: (String val) => setState(() => email = val),
+                            onEditingComplete: () => node.nextFocus(),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                          VerticalSizedBox16(),
+                          TextFormField(
+                            controller: passwordController,
+                            decoration: textInputDecoration.copyWith(hintText: 'Hasło'),
+                            obscureText: true,
+                            validator: (val) => val!.length < 6 ? 'Hasło musi zawierać minimum 6 znaków' : null,
+                            onEditingComplete: () => registerWithEmailAndPassword(),
+                          ),
+                          VerticalSizedBox16(),
+                          TextFormField(
+                            controller: passwordRepeatController,
+                            decoration: textInputDecoration.copyWith(hintText: 'Powtórz hasło'),
+                            obscureText: true,
+                            validator: (val) => val != passwordController.text ? 'Podane hasła nie są zgodne' : null,
+                            onEditingComplete: () => registerWithEmailAndPassword(),
+                          ),
+                          VerticalSizedBox16(),
+                          CustomElevatedButton(
+                            label: 'Utwórz nowe konto',
+                            onPressed: registerWithEmailAndPassword,
+                          ),
+                          if (error.isNotEmpty) VerticalSizedBox16(),
+                          Text(
+                            error,
+                            style: Theme.of(context).textTheme.bodyText2!.copyWith(color: Palette.errorRed),
+                          ),
+                          if (error.isNotEmpty) VerticalSizedBox16(),
+                          CustomTextButton(
+                            onPressed: widget.toggleView,
+                            label: 'Masz już konto? Zaloguj się.',
+                            color: Palette.ivoryBlack,
+                          )
+                        ],
+                      )),
+                ),
+              ),
+            )),
           );
   }
 }
