f7861a8cb926ef1928dce39a76631f25e75e6d3e
Arthur <artoni.az@gmail.com>
2021-12-04 12:16:39 +0100

    Added timeago package

--- lib/config/functions/timestamp_methods.dart
+++ lib/config/functions/timestamp_methods.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 String timestampToDate(Timestamp timestamp) => DateFormat('yyyy-MM-dd').format(getDateTimeFromTimestamp(timestamp));
 
@@ -10,3 +11,5 @@ int calcDaysBetween(Timestamp timestamp) {
   DateTime from = getDateTimeFromTimestamp(timestamp);
   return DateTime.now().difference(from).inDays;
 }
+
+String getTimeagoFromDateTime(Timestamp timestamp) => timeago.format(getDateTimeFromTimestamp(timestamp), locale: 'pl');
